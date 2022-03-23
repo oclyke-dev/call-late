8273ab5c0edc119524726c87ecde03f845b0e22c
Owen <o@oclyke.dev>
2022-03-22 23:33:56 -0700

    improve test fixture
    * properly close client connection
    * properly shut down mongo memory server

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,45 +1,19 @@
-import { MongoMemoryReplSet } from 'mongodb-memory-server';
-
 import {
-  MongoClient,
-  Db,
-  Collection,
-} from 'mongodb';
+  MongoMemoryReplSet
+} from 'mongodb-memory-server';
 
-export async function connect() {
-  return new Promise<MongoClient>((resolve, reject) => {
+export async function start_server() {
+  return new Promise<MongoMemoryReplSet>((resolve, reject) => {
     if(process.env.NODE_ENV === 'production'){
-      reject('not ready for production!');
-    } else if ((process.env.NODE_ENV === 'development') || (process.env.NODE_ENV === 'test')) {
-
+      reject('not to be used in production');
+    } else if ((process.env.NODE_ENV === 'development') || (process.env.NODE_ENV === 'test')){
       // see docs for mongodb-memory-server
       // https://github.com/nodkz/mongodb-memory-server
       MongoMemoryReplSet.create({ replSet: { count: 4 } })
-      .then(replset => {
-        const uri = replset.getUri();
-        const client = new MongoClient(uri);
-        return client.connect();
-      })
-      .then(client => {
-        resolve(client);
-      })
+      .then(resolve)
       .catch(reject);
-      
     } else {
       reject(`invalid NODE_ENV: ${process.env.NODE_ENV}`);
     }
-  })
+  });
 }
-
-export async function disconnect() {
-  return new Promise<void>((resolve, reject) => {
-    if(process.env.NODE_ENV === 'production'){
-      reject('not ready for production!');
-    } else if ((process.env.NODE_ENV === 'development') || (process.env.NODE_ENV === 'test')) {
-      resolve();
-    } else {
-      reject(`invalid NODE_ENV: ${process.env.NODE_ENV}`);
-    }
-  })
-}
-
--- backend/test/db.test.ts
+++ backend/test/db.test.ts
@@ -1,30 +1,31 @@
 import {
-  connect,
-  disconnect,
+  start_server,
 } from '../src/utils';
+
+import {
+  MongoMemoryReplSet
+} from 'mongodb-memory-server';
 
 import {
   MongoClient
 } from 'mongodb';
 
 let client: MongoClient;
+let replset: MongoMemoryReplSet;
 
 // see docs on jest setup / teardown as well as handling async code
 // https://jestjs.io/docs/setup-teardown
 // https://jestjs.io/docs/asynchronous
-beforeAll(() => {
-  return new Promise<void>((resolve, reject) => {
-    connect()
-    .then(c => {
-      client = c;
-      resolve();
-    })
-    .catch(reject);
-  });
+beforeAll(async () => {
+  replset = await start_server();
+  const uri = replset.getUri();
+  client = new MongoClient(uri);
+  await client.connect();
 });
 
-afterAll(() => {
-  return disconnect();
+afterAll(async () => {
+  await client.close();
+  await replset.stop();
 });
 
 test('this test to make the test suite valid', () => {
