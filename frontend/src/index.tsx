f9ca71cf758c246651a9c96336302bb1e2da9bf5
Owen <o@oclyke.dev>
2022-03-24 23:09:26 -0700

    add frontend foundation
    a simple snowpack + react getup

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  default as App,
+} from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
