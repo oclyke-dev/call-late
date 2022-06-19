707d57b68873906450739b806fd0ab1538822a10
Owen <o@oclyke.dev>
2022-06-18 23:28:29 -0700

    create theme
    and rely on mui css baseline to clear out css defaults - this also means we can get rid of our index.css stylesheet
    background color is provided in the mui theme palette

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,6 +10,15 @@ import {
 } from 'react-router-dom';
 
 import {
+  ThemeProvider,
+} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import {
+  theme,
+} from './theme';
+
+import {
   Game,
   Start,
   User,
@@ -24,15 +33,18 @@ import {
 const container = document.getElementById('root');
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}>
-          <Route path='$user' element={<User />} />
-          <Route path='$leaderboard' element={<Leaderboard />} />
-          <Route path=':tag' element={<Game />} />
-          <Route index element={<Start />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route path='$user' element={<User />} />
+            <Route path='$leaderboard' element={<Leaderboard />} />
+            <Route path=':tag' element={<Game />} />
+            <Route index element={<Start />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
   , container);
---
+++ frontend/src/theme.tsx
@@ -0,0 +1,74 @@
+import {
+  createTheme,
+} from '@mui/material/styles';
+
+import {
+  orange,
+  blue,
+} from '@mui/material/colors';
+
+// // example of how the 'status' field could be employed to theme an element
+// const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
+//   color: theme.status.ok,
+//   '&.Mui-checked': {
+//     color: theme.status.danger,
+//   },
+// }));
+// render(<CustomCheckbox/>)
+
+declare module '@mui/material/styles' {
+  interface Theme {
+
+    // adding this status field shows how the theme can be extended with custom functionality - the app does not currently use the 'status' options
+    status: {
+      danger: string;
+      ok: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+
+    // as with above this enables the 
+    status?: {
+      danger?: string;
+      ok?: string;
+    };
+  }
+}
+
+// app fonts
+const app_fonts = [
+  'Comic Neue',
+  'Roboto',
+  'Arial',
+];
+
+// normal theme
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: '#d3def0',
+    },
+  },
+  typography: {
+    fontFamily: app_fonts.join(','),
+
+    h1: {
+      fontFamily: [
+        'Mali',
+        // 'Shadows Into Light Two',
+        // 'Architects Daughter',
+        // 'Schoolbell',
+        ...app_fonts,
+      ].join(','),
+    },
+  },
+
+  // here is how the coors for various statuses are encoded in the theme
+  status: {
+    danger: orange[500],
+    ok: blue[300],
+  },
+});
+
+
