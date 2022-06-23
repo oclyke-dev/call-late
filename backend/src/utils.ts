6dbf345643a99e1c96d2ae9c61f3fff38e97787d
Owen <o@oclyke.dev>
2022-06-23 01:32:59 -0700

    allow memory database with flag

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -12,19 +12,21 @@ import {
   Hand,
 } from '.';
 
+function memorydb_allowed() {
+  return (typeof process.env.ALLOW_MEMORY_DB !== 'undefined') ? true : false;
+}
+
 export async function start_server() {
   return new Promise<MongoMemoryReplSet>((resolve, reject) => {
-    if(process.env.NODE_ENV === 'production'){
-      reject('not to be used in production');
-    } else if ((process.env.NODE_ENV === 'development') || (process.env.NODE_ENV === 'test')){
-      // see docs for mongodb-memory-server
-      // https://github.com/nodkz/mongodb-memory-server
-      MongoMemoryReplSet.create({ replSet: { count: 4 } })
-      .then(resolve)
-      .catch(reject);
-    } else {
-      reject(`invalid NODE_ENV: ${process.env.NODE_ENV}`);
+    if((process.env.NODE_ENV === 'production') && (!memorydb_allowed())){
+      reject('memory server not allowed');
     }
+
+    // see docs for mongodb-memory-server
+    // https://github.com/nodkz/mongodb-memory-server
+    MongoMemoryReplSet.create({ replSet: { count: 4 } })
+    .then(resolve)
+    .catch(reject);
   });
 }
 
